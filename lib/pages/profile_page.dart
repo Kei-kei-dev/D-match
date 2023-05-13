84897e04e0e69be5feebb2991249870ab744da34
Kei-kei-dev <124547360+Kei-kei-dev@users.noreply.github.com>
2023-05-13 20:26:43 +0900

    静的なUI作成完了

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -12,9 +12,43 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('チャットアプリ'),
+        title: const Text('プロフィール編集'),
       ),
-      body: const Center(child: Text('プロフィール画面です')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            // ここだけ教材とconstの位置が違う
+            const Row(
+              children: [
+                SizedBox(width: 150, child: Text('名前')),
+                Expanded(child: TextField())
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                const SizedBox(width: 150, child: Text('プロフィール画像')),
+                Expanded(
+                  child: Container(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text('画像を選択'))),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            SizedBox(
+                width: 150,
+                child:
+                    ElevatedButton(onPressed: () {}, child: const Text('編集')))
+          ],
+        ),
+      ),
     );
   }
 }
